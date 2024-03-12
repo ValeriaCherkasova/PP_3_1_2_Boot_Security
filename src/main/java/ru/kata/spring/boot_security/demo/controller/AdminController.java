2a5cde7e6fa09940a1ef56025e2191388e0b1720
ValeriaCherkasova <vc600139@gmail.com>
2024-03-12 13:17:29 +0300

    4

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,9 +1,7 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.kata.spring.boot_security.demo.model.User;
@@ -19,25 +17,6 @@ public class AdminController {
         List<User> users = userService.getAllUsers();
         model.addAttribute("users", users);
         return "users";
-    }
-
-    @GetMapping(value = "/read_user")
-    public String readUser(@RequestParam("id") Long id, Model model) {
-        User user = userService.readUser(id);
-        model.addAttribute("user", user);
-        return "read_user";
-    }
-
-    @PostMapping(value = "/create_user")
-    public String createUser(@ModelAttribute("user") User user) {
-        userService.createUser(user);
-        return "redirect:/user/get_all_users";
-    }
-
-    @PostMapping(value = "/update_user")
-    public String updateUser(User user) {
-        userService.updateUser(user);
-        return "redirect:/user/get_all_users";
     }
 
     @PostMapping(value = "/delete_user")
